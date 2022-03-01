2abd816415d7cfaf0e2d53c1b84371f015c75ab6
Krizsán Péter <krizsan.peter@gmail.com>
2022-03-01 16:25:58 +0100

    Added FreeRDP package.

--- add_packages.sh
+++ add_packages.sh
@@ -1,15 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 
-cp -v libtorrent-rasterbar-2.0.5-update-allocator-sizes-for-boost-1.78.patch /sources
-cp -v networking/netlibs/libtorrent-rasterbar.xml ../blfs_root/blfs-xml/networking/netlibs
-cp -v xsoft/other/qbittorrent.xml ../blfs_root/blfs-xml/xsoft/other
-cp -v multimedia/libdriv/libid3tag.xml ../blfs_root/blfs-xml/multimedia/libdriv
-cp -v server/other/minidlna.xml ../blfs_root/blfs-xml/server/other
-sed -i '/libtirpc.xml.*/a <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="libtorrent-rasterbar.xml"/>' ../blfs_root/blfs-xml/networking/netlibs/netlibs.xml
-sed -i '/pidgin.xml.*/a <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="qbittorrent.xml"/>' ../blfs_root/blfs-xml/xsoft/other/other.xml
-sed -i '/libmad.xml.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="libid3tag.xml"/>' ../blfs_root/blfs-xml/multimedia/libdriv/libdriv.xml
-sed -i '/openldap.xml.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="minidlna.xml"/>' ../blfs_root/blfs-xml/server/other/other.xml
-sed -i '/libtirpc-version.*/a <!ENTITY libtorrent-rasterbar-version "2.0.5">' ../blfs_root/blfs-xml/packages.ent
-sed -i '/pidgin-version.*/a <!ENTITY qbittorrent-version "4.4.1">' ../blfs_root/blfs-xml/packages.ent
-sed -i '/libmad-version.*/i <!ENTITY libid3tag-version "0.15.1b">' ../blfs_root/blfs-xml/packages.ent
-sed -i '/openldap-version.*/i <!ENTITY minidlna-version "1.3.0">' ../blfs_root/blfs-xml/packages.ent
+cp -fv libtorrent-rasterbar-2.0.5-update-allocator-sizes-for-boost-1.78.patch /sources
+cp -fv networking/netlibs/libtorrent-rasterbar.xml ../blfs_root/blfs-xml/networking/netlibs
+cp -fv xsoft/other/qbittorrent.xml ../blfs_root/blfs-xml/xsoft/other
+cp -fv xsoft/other/freerdp.xml ../blfs_root/blfs-xml/xsoft/other
+cp -fv multimedia/libdriv/libid3tag.xml ../blfs_root/blfs-xml/multimedia/libdriv
+cp -fv server/other/minidlna.xml ../blfs_root/blfs-xml/server/other
+grep -qF libtorrent-rasterbar.xml ../blfs_root/blfs-xml/networking/netlibs/netlibs.xml || sed -i '/<\/chapter>.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="libtorrent-rasterbar.xml"/>' ../blfs_root/blfs-xml/networking/netlibs/netlibs.xml
+grep -qF freerdp.xml ../blfs_root/blfs-xml/xsoft/other/other.xml || sed -i '/<\/chapter>.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="freerdp.xml"/>' ../blfs_root/blfs-xml/xsoft/other/other.xml
+grep -qF qbittorrent.xml ../blfs_root/blfs-xml/xsoft/other/other.xml || sed -i '/<\/chapter>.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="qbittorrent.xml"/>' ../blfs_root/blfs-xml/xsoft/other/other.xml
+grep -qF libid3tag.xml ../blfs_root/blfs-xml/multimedia/libdriv/libdriv.xml || sed -i '/<\/chapter>.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="libid3tag.xml"/>' ../blfs_root/blfs-xml/multimedia/libdriv/libdriv.xml
+grep -qF minidlna.xml ../blfs_root/blfs-xml/server/other/other.xml || sed -i '/<\/chapter>.*/i <xi:include xmlns:xi="http://www.w3.org/2001/XInclude" href="minidlna.xml"/>' ../blfs_root/blfs-xml/server/other/other.xml
+grep -qF libtorrent-rasterbar-version ../blfs_root/blfs-xml/packages.ent || echo '<!ENTITY libtorrent-rasterbar-version "2.0.5">' >> ../blfs_root/blfs-xml/packages.ent
+grep -qF freerdp-version ../blfs_root/blfs-xml/packages.ent || echo '<!ENTITY freerdp-version "2.6.0">' >> ../blfs_root/blfs-xml/packages.ent
+grep -qF qbittorrent-version ../blfs_root/blfs-xml/packages.ent || echo '<!ENTITY qbittorrent-version "4.4.1">' >> ../blfs_root/blfs-xml/packages.ent
+grep -qF libid3tag-version ../blfs_root/blfs-xml/packages.ent || echo '<!ENTITY libid3tag-version "0.15.1b">' >> ../blfs_root/blfs-xml/packages.ent
+grep -qF minidlna-version ../blfs_root/blfs-xml/packages.ent || echo '<!ENTITY minidlna-version "1.3.0">' >> ../blfs_root/blfs-xml/packages.ent
