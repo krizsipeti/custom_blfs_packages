6b4ea072547b15aa100e2b78c3a7e8bd71ffd12f
Krizsan Peter <krizsan.peter@gmail.com>
2024-03-21 22:49:18 +0100

    Script file to prevent upgrading existing packages

---
+++ rmscripts.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# first parameter is the blfs tool's script folder
+# second parameter is the blfs tool's installed packages xml file
+
+if [ -z "$1" ]
+then
+	echo "Please specify the blfs tool's script folder as the first argument!"
+	exit -1
+fi
+
+if [ -z "$2" ]
+then
+        echo "Please specify the blfs tool's installed packages xml file as the second argument!"
+	exit -1
+fi
+
+if [[ "$1" != */ ]]
+then
+	SCRIPTPATH=$(echo "$1"/)
+else
+	SCRIPTPATH="$1"
+fi
+
+grep "$2" -f <(ls "$SCRIPTPATH" | awk -F'-z-' '{print "<name>"$2"</name>"}') | while read line ; do rm -v "$SCRIPTPATH"$(echo "$line" | sed 's/<name>/*-/g' | sed 's|</name>||g') ; done
+
