f3655a14e8a21fb81c47de0e8c71227fb3d97b28
Krizsán Péter <krizsipeti@freemail.hu>
2024-10-29 22:18:33 +0100

    Added vscode.sh to profile.d folder

---
+++ lfs_configs/etc/profile.d/vscode.sh
@@ -0,0 +1,10 @@
+# Begin /etc/profile.d/vscode.sh
+
+VSCODEDIR=/opt/vscode
+
+pathappend $VSCODEDIR/bin           PATH
+#pathappend $VSCODEDIR/lib/pkgconfig PKG_CONFIG_PATH
+
+export VSCODEDIR
+
+# End /etc/profile.d/vscode.sh
