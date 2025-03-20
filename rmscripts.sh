0d118160bfd48afd195a3d3d455b6704ad42562a
pkr <krizsan.peter@gmail.com>
2025-03-20 23:41:55 +0100

    Fix grammatical errors in the echo statements

--- rmscripts.sh
+++ rmscripts.sh
@@ -6,26 +6,26 @@ set -e
 
 # Check if the script folder is specified as an argument
 if [ -z "$1" ] ; then
-    echo "Please specify the blfs tool's script folder as the first argument!" >&2
+    echo "Please specify the BLFS tool's script folder as the first argument." >&2
     exit 1
 fi
 
 # Check if the given script folder exists
 if [ ! -d "$1" ] ; then
-    echo "The given script folder is not exist!" >&2
-	exit 2
+    echo "The specified script folder does not exist." >&2
+    exit 2
 fi
 
-# Check if the installed packages xml is specified as an argument
+# Check if the installed packages XML file is specified as an argument
 if [ -z "$2" ] ; then
-    echo "Please specify the blfs tool's installed packages xml file as the second argument!" >&2
+    echo "Please specify the BLFS tool's installed packages XML file as the second argument." >&2
     exit 3
 fi
 
-# Check if installed packages xml file exists
+# Check if the installed packages XML file exists
 if [ ! -f "$2" ] ; then
-    echo "The given installed packages xml file is not exist!" >&2
-	exit 4
+    echo "The specified installed packages XML file does not exist." >&2
+    exit 4
 fi
 
 # Add slash to script path if not there yet
@@ -35,9 +35,9 @@ else
     SCRIPTPATH="$1"
 fi
 
-# Find and remove all the scripts which would install a package that is already mentioned in the installed packages file
+# Find and remove all the scripts that would install a package already mentioned in the installed packages file
 grep "$2" -f <(find "$SCRIPTPATH" -type f -printf '%f\n' | awk -F'-z-' '{print "<name>"$2"</name>"}') | \
-	while read -r line
-	do
-	    rm -v "$(find "$SCRIPTPATH" -name "$(echo "$line" | sed 's/<name>/*-/g' | sed 's|</name>||g')" -type f)"
-	done
+    while read -r line
+    do
+        rm -v "$(find "$SCRIPTPATH" -name "$(echo "$line" | sed 's/<name>/*-/g' | sed 's|</name>||g')" -type f)"
+    done
